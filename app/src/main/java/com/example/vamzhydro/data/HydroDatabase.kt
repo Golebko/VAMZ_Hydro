a825a2555df5420c0a15766d510f5a1e9e1c5ffa
Golebko <85166851+Golebko@users.noreply.github.com>
2024-05-12 15:58:12 +0200

    creation of HydroDatabase

---
+++ app/src/main/java/com/example/vamzhydro/data/HydroDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.vamzhydro.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Record::class], version = 1, exportSchema = false)
+abstract class HydroDatabase : RoomDatabase() {
+
+    abstract fun Dao(): Dao
+
+    companion object {
+        @Volatile
+        private var Instance: HydroDatabase? = null
+
+        fun getDatabase(context: Context): HydroDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, HydroDatabase::class.java, "record_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
