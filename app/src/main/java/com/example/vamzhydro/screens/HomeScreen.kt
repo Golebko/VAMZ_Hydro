a8c5fb482e6cc2007b6ac0e63368c65be734d48b
Golebko <85166851+Golebko@users.noreply.github.com>
2024-06-06 18:18:38 +0200

    home screen viewmodel

--- app/src/main/java/com/example/vamzhydro/screens/HomeScreen.kt
+++ app/src/main/java/com/example/vamzhydro/screens/HomeScreen.kt
@@ -27,6 +27,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -43,6 +45,60 @@ import com.example.vamzhydro.data.RecordItem
 import com.example.vamzhydro.navigation.NavigationRoute
 import com.example.vamzhydro.ui.theme.AppTheme
 
+object HomeScreen : NavigationRoute {
+    override val route = "home"
+    override val title = R.string.app_name
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    navigateToItemUpdate: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel()
+) {
+    val homeState by viewModel.homeState.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopBar(
+                title = stringResource(HomeScreen.title),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        bottomBar = {
+            BottomNavBar()
+
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .padding(
+                        end = WindowInsets.safeDrawing.asPaddingValues()
+                            .calculateEndPadding(LocalLayoutDirection.current)
+                    )
+            ) {
+                /*Icon(
+                    imageVector = Icons.Default.Add,
+                    //contentDescription = stringResource(R.string.item_entry_title)
+                )*/
+            }
+        },
+    ) { innerPadding ->
+        HomeBody(
+            itemList = homeState.itemList,
+            onItemClick = navigateToItemUpdate,
+            modifier = modifier.fillMaxSize(),
+            contentPadding = innerPadding,
+        )
+    }
+}
 
 @Composable
 private fun HomeBody(
--- app/src/main/java/com/example/vamzhydro/screens/HomeViewModel.kt
+++ app/src/main/java/com/example/vamzhydro/screens/HomeViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.vamzhydro.screens
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vamzhydro.data.RecordItem
+import com.example.vamzhydro.data.RecordsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class HomeViewModel : ViewModel() {
+
+data class HomeState(val itemList: List<RecordItem> = listOf())
+class HomeViewModel(repository: RecordsRepository) : ViewModel() {
+
+
+    val homeState: StateFlow<HomeState> = repository.getAllRecordsStream().map { HomeState(it) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = HomeState()
+        )
+
 }
+
